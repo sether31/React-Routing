09ba760168fb74bae922fd58e42ffc2bc0fdd9eb
Seth Hernandez <sethmichaelhernandez@gmail.com>
2025-09-28 09:19:24 +0800

    feat(logout): Add logout using navigate with replace
    - Use <Navigate/> component to  redirect the user back to home page, while also setting it to replace "true" to replace the current entry in the browser's history stack.

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,7 +1,24 @@
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
 function About() {
+  const [user, setUser] = useState('sether')
+
+  if(!user){
+    return <Navigate to="/" replace={true} />
+  }
+
   return(
     <section>
       <h2 className="text-3xl">About Page</h2>
+      <p className="my-8">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis ducimus minus, itaque repellendus aut est fuga dolor non, molestias laborum nihil asperiores nam unde magnam odit voluptatem dicta quod, eaque esse mollitia nulla repellat qui? Cumque quidem, dolorem molestiae, doloribus harum ipsum enim nobis facilis, hic fugiat illum cupiditate perferendis.
+      </p>
+      <button 
+        className="px-4 py-2 text-white bg-blue-500 rounded-sm hover:bg-blue-700"
+        onClick={() => setUser(null)}
+      >
+        Logout
+      </button>
     </section>
   )
 }
